d2ad7c6505e27dd905c2a75ed98d90721c23244d
SIGMASIRRRE <dealbot612@gmail.com>
2025-04-15 10:29:56 +0500

    Add files via upload

---
+++ src/data/hallOfFame.js
@@ -0,0 +1,12 @@
+export const hallOfFame = [
+  {
+    name: "Stony",
+    message: "Creator of the best PvP scanner!",
+    image: "Stony"
+  },
+  {
+    name: "xicz",
+    message: "Legend of NethPot!",
+    image: "xicz"
+  }
+]
---
+++ src/data/players.js
@@ -0,0 +1,28 @@
+export const players = {
+  "Stony": {
+    name: "Stony",
+    message: "One of the best NethPot players!",
+    tiers: {
+      "Netherite Potion": "HT1",
+      "Diamond Pot": "HT2",
+      "Sword": "LT1",
+      "Axe": "HT3",
+      "SMP": "HT1",
+      "Cpvp": "HT2",
+      "UHC": "LT2"
+    }
+  },
+  "xicz": {
+    name: "xicz",
+    message: "Top 1 PVPer.",
+    tiers: {
+      "Netherite Potion": "HT1",
+      "Diamond Pot": "HT1",
+      "Sword": "HT1",
+      "Axe": "HT2",
+      "SMP": "HT3",
+      "Cpvp": "HT1",
+      "UHC": "HT2"
+    }
+  }
+}
